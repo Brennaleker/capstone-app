1eb5dbde14560a49aef405b4baef8d041bd2c305
Brenna <brennaleker@gmail.com>
2015-10-19 16:23:09 -0700

    WIP - Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,7 @@ class HomeController < ApplicationController
     organizations
     projects
     gifts
+    donors
   end
 
   def profile
@@ -17,31 +18,51 @@ class HomeController < ApplicationController
   private
 
   def users
-    @users_result = HTTParty.get('http://localhost:3001/users')
+    @users_result = HTTParty.get('http://localhost:3001/users')["data"]
+  end
+
+  def donors
+    @donors_count = 0
+    @users_result.each do |user|
+      if user["donor"] == true
+        @donors_count += 1
+      end
+    end
   end
 
   def volunteers
-    @volunteers_result = HTTParty.get('http://localhost:3001/volunteers')
-    @volunteer_count = @volunteers_result.count
-    @volunteers_pending = HTTParty.get('http://localhost:3001/volunteers/pending')
+    @volunteers_result = HTTParty.get('http://localhost:3001/volunteers')["data"]
+    @volunteers_count = @volunteers_result.count
+    @volunteers_pending = HTTParty.get('http://localhost:3001/volunteers/pending')["data"]
   end
 
   def organizations
-    @organizations_result = HTTParty.get('http://localhost:3001/organizations')
+    @organizations_result = HTTParty.get('http://localhost:3001/organizations')["data"]
     @organizations_count = @organizations_result.count
   end
 
   def projects
-    @projects_result =  HTTParty.get('http://localhost:3001/projects')
-    @projects_pending =  HTTParty.get('http://localhost:3001/projects/pending')
-    @projects_approved =  HTTParty.get('http://localhost:3001/projects/pending')
-    @projects_funded =  HTTParty.get('http://localhost:3001/projects/funded')
-    @funded_count = @projects_funded.count
+    @projects_result =  HTTParty.get('http://localhost:3001/projects')["data"]
+    @projects_pending =  HTTParty.get('http://localhost:3001/projects/pending')["data"]
+    @projects_approved =  HTTParty.get('http://localhost:3001/projects/pending')["data"]
+    @projects_approved_count = @projects_approved.count
+    @projects_funded =  HTTParty.get('http://localhost:3001/projects/funded')["data"]
+    @projects_funded_count = @projects_funded.count
   end
 
   def gifts
-    @gifts_result = HTTParty.get('http://localhost:3001/gifts')
+    @gifts_result = HTTParty.get('http://localhost:3001/gifts')["data"]
     @donations_count = @gifts_result.count
+    total_gifted
+  end
+
+  def total_gifted
+    @total_gifted = 0
+    @gifts_result.each do |gift|
+      if gift["status"] == "approved"
+        @total_gifted += gift["amount"]
+      end
+    end
   end
 
   # def retrieve_profile =
